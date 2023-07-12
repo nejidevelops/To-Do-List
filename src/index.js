fdff5845bd472cfb05f021b62b19bd95f19a4205
Newton Ombese <newtonombese1@gmail.com>
2023-07-12 09:23:23 +0300

    Add my index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './style.css';
+
+const tasks = [
+  { description: 'Task 1', completed: false, index: 1 },
+  { description: 'Task 2', completed: true, index: 2 },
+  { description: 'Task 3', completed: false, index: 3 },
+];
+
+function renderTasks() {
+  const todoList = document.querySelector('.to-do');
+
+  tasks.sort((a, b) => a.index - b.index);
+
+  todoList.innerHTML = '';
+
+  tasks.forEach((task) => {
+    const listItem = document.createElement('li');
+    listItem.textContent = task.description;
+    if (task.completed) {
+      listItem.classList.add('completed');
+    }
+    todoList.appendChild(listItem);
+  });
+}
+
+renderTasks();
