f3b66670988c98f5f659676522a3adc97e0d760e
Newton Ombese <newtonombese1@gmail.com>
2023-07-12 12:15:31 +0300

    Add my updated index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import './style.css';
 
-const tasks = [
-  { description: 'Task 1', completed: false, index: 1 },
-  { description: 'Task 2', completed: true, index: 2 },
-  { description: 'Task 3', completed: false, index: 3 },
-];
+let tasks = [];
 
 function renderTasks() {
   const todoList = document.querySelector('.to-do');
@@ -15,12 +11,103 @@ function renderTasks() {
 
   tasks.forEach((task) => {
     const listItem = document.createElement('li');
-    listItem.textContent = task.description;
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.addEventListener('change', () => {
+      task.completed = checkbox.checked;
+      renderTasks();
+    });
+
+    const taskDescription = document.createElement('span');
+    taskDescription.textContent = task.description;
+    taskDescription.contentEditable = true;
+    taskDescription.addEventListener('input', () => {
+      task.description = taskDescription.textContent.trim();
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.addEventListener('click', () => {
+      tasks = tasks.filter((t) => t !== task);
+      renderTasks();
+    });
+
+    const hr = document.createElement('hr');
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskDescription);
+    listItem.appendChild(deleteButton);
+    todoList.appendChild(hr);
+
     if (task.completed) {
       listItem.classList.add('completed');
     }
+
     todoList.appendChild(listItem);
   });
 }
 
 renderTasks();
+
+function addTask(event) {
+  if (event) {
+    event.preventDefault();
+  }
+
+  const taskInput = document.getElementById('task-input');
+  const taskDescription = taskInput.value.trim();
+
+  if (taskDescription !== '') {
+    const newTask = {
+      description: taskDescription,
+      completed: false,
+      index: tasks.length + 1,
+    };
+
+    tasks.push(newTask);
+    renderTasks();
+
+    taskInput.value = '';
+  }
+}
+
+addTask();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const taskForm = document.getElementById('task-form');
+
+  taskForm.addEventListener('submit', addTask);
+
+  const taskInput = document.getElementById('task-input');
+  taskInput.addEventListener('keydown', (event) => {
+    if (event.keyCode === 13) {
+      addTask(event);
+    }
+  });
+
+  renderTasks();
+});
+
+function removeAllTasks() {
+  const deleteAll = document.querySelector('.refresh');
+
+  deleteAll.addEventListener('click', () => {
+    tasks = [];
+    renderTasks();
+  });
+}
+
+removeAllTasks();
+
+function removeCompletedTasks() {
+  const removeCompleted = document.querySelector('.clear-complete-tasks');
+
+  removeCompleted.addEventListener('click', () => {
+    tasks = tasks.filter((task) => !task.completed);
+    renderTasks();
+  });
+}
+
+removeCompletedTasks();
