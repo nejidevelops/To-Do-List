57ab585859cb6a56b1fd1ad46864cb5070ff23f4
Newton Ombese <newtonombese1@gmail.com>
2023-07-20 15:31:00 +0300

    Add renderTask.js file

---
+++ src/renderTask.js
@@ -0,0 +1,52 @@
+/* eslint-disable import/no-cycle */
+/* eslint-disable import/prefer-default-export */
+/* eslint-disable no-undef */
+import { deleteTask } from './deleteTask.js';
+import { saveTasks } from './index.js';
+import { updateTaskStatus } from '../modules/taskStatus.js';
+
+export function renderTasks(tasks) {
+  const todoList = document.querySelector('.to-do');
+
+  tasks.sort((a, b) => a.index - b.index);
+
+  todoList.innerHTML = '';
+
+  tasks.forEach((task, taskIndex) => {
+    const listItem = document.createElement('li');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.checked = task.completed;
+    checkbox.addEventListener('change', () => {
+      updateTaskStatus(tasks, taskIndex, checkbox.checked);
+    });
+
+    const taskDescription = document.createElement('span');
+    taskDescription.textContent = task.description;
+    taskDescription.contentEditable = true;
+    taskDescription.addEventListener('input', () => {
+      task.description = taskDescription.textContent.trim();
+      saveTasks();
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+    deleteButton.addEventListener('click', () => {
+      deleteTask(task, tasks);
+    });
+
+    const hr = document.createElement('hr');
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(taskDescription);
+    listItem.appendChild(deleteButton);
+    todoList.appendChild(hr);
+
+    if (task.completed) {
+      listItem.classList.add('completed');
+    }
+
+    todoList.appendChild(listItem);
+  });
+}
